e63518b5d057723407b681843d98baccf3033403
Yria Forján Oliveira <yriaforjan.oliveira@gmail.com>
2025-11-19 21:50:11 +0100

    refactor(routes/user): Remove redundant /update and /delete in routes

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,14 +1,19 @@
 const { isAdmin } = require("../../middlewares/isAdmin");
 const { isAuth } = require("../../middlewares/isAuth");
-const { register, login, getUsers, updateUser, deleteUser } = require("../controllers/user");
+const {
+  register,
+  login,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user");
 
 const usersRouter = require("express").Router();
 
 usersRouter.post("/register", register);
 usersRouter.post("/login", login);
-usersRouter.get("/", isAuth, getUsers)
-usersRouter.put("/update/:id", isAuth, isAdmin, updateUser)
-usersRouter.delete("/delete/:id", isAuth, isAdmin, deleteUser)
-
+usersRouter.get("/", isAuth, getUsers);
+usersRouter.put("/:id", isAuth, isAdmin, updateUser);
+usersRouter.delete("/:id", isAuth, isAdmin, deleteUser);
 
 module.exports = { usersRouter };
