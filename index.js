1a9a40ee06147652076e85dd0d65ce739530ca7d
Cvidal84 <carlosvidalpuigcerver@gmail.com>
2026-03-17 16:15:59 +0100

    fix: add fallback port for production

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   return res.status(404).json("Route not found ❌");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor levantado en http://localhost:${PORT} ✅`);
 });
